fd32e44fac81ef4b29f326235be51c3a34e36c64
Juan F. Codagnone <juan@zauberlabs.com>
2003-02-08 13:36:06 +0000

    soporte debug

--- src/link.h
+++ src/link.h
@@ -10,6 +10,7 @@ typedef struct link_parserCDT * link_parser_t;
 
 
 link_parser_t link_parser_new(void);
+void link_parser_set_debug(link_parser_t parser, int b);
 void link_parser_destroy(link_parser_t parser);
 void link_parser_set_link_callback(link_parser_t parser,link_callback call,
                                     void *d );
