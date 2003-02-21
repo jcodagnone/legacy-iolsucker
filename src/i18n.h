5c7d7e981e36229dbe214618e5b8709d4f09261d
Juan F. Codagnone <juan@zauberlabs.com>
2003-02-21 16:17:12 +0000

    more macros

--- src/i18n.h
+++ src/i18n.h
@@ -1,5 +1,9 @@
 #ifndef _I18N_
 #define _I18N_
 
-#define _(a) (a)
+#define _(String) (String)
+#define N_(String) (String)
+#define textdomain(Domain)
+#define bindtextdomain(Package, Directory)
+
 #endif
