2abbd71a96203616b190255fa298da1eda8fc2d9
Juan F. Codagnone <juan@zauberlabs.com>
2003-02-23 17:18:55 +0000

    fix licencia

--- src/main.c
+++ src/main.c
@@ -4,14 +4,14 @@
  * Copyright (C) 2003 by Juan F. Codagnone <juam@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
@@ -98,6 +98,7 @@ parseOptions( int argc, char * const * argv, struct opt *opt)
 	i = GetOptions( argv, lopt, 0, NULL);
 	if( i < 0 )
 	{	rs_log_error("parsing options");
+		usage();
 		return -1;
 	}
 
