8d763f8c698def95f3c21e48e5ba3e787687ad4f
Juan F. Codagnone <juan@zauberlabs.com>
2003-02-27 18:49:26 +0000

    *** empty log message ***

---
+++ src/fconfig.h
@@ -0,0 +1,10 @@
+#ifndef Z40E93959D74A919952A997120A9E2296
+#define Z40E93959D74A919952A997120A9E2296
+
+#include "main.h"
+
+int
+load_config_file(struct opt *opt);
+
+
+#endif
---
+++ src/iolerr.c
@@ -0,0 +1,35 @@
+#include <iol.h>
+
+const char *
+iol_strerror(int code)
+{	const char *r;
+
+	switch(code)
+	{
+		case E_INVAL:
+			r = "invalid arguments";
+			break;
+		case E_ALOGED:
+			r = "already logged";
+			break;
+		case E_NLOGED:
+			r = "not logued";
+			break;
+		case E_NETWORK:
+			r = "network error";
+			break;
+		case E_MEMORY:
+			r = "need to take some Memorol 500 pills :)";
+			break;
+		case E_FS:
+			r = "error in the filesystem I/O";
+			break;
+		case E_MAXERROR:
+			r = "E_MAXERROR";
+			break;
+		default:
+			r = "";
+	}
+
+	return r;
+}
