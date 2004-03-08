d6cb0e3457b476bc26ffe1b47f274d6d7497747e
Juan F. Codagnone <juan@zauberlabs.com>
2004-03-08 05:01:12 +0000

    - sigo reacomando macros  - muevo Makefile.cvs a autobuild.sh (parece ser mas comun hoy en dia)

---
+++ autobuild.sh
@@ -0,0 +1,18 @@
+MAKE=make
+
+echo "*** Retrieving configure tests needed by configure.in"
+aclocal -I . -I admin -I admin/extra
+echo "*** Scanning for include statements"
+autoheader
+echo "*** Building Makefile templates (step one)"
+automake -a
+echo "*** Building Makefile templates (step two)"
+autoconf
+if grep "ac_kw foo" configure &>/dev/null; then perl -p -i -e "s/ac_kw foo/ac_kw int foo/" configure; fi
+perl -pi -e 'if (/\[\/\$$\]\*. INSTALL=/) { print $$_ ; $$_ = "\"\") ;;\n"; }' configure
+rm -f config.cache config.h
+echo "*** Create date/time stamp"
+touch stamp-h.in
+echo "*** Finished"
+echo "    Don't forget to run ./configure"
+echo "    If you haven't done so in a while, run ./configure --help"
