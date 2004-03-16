e045b21ea30addecbe3ba76bf116606d9058b6b1
Juan F. Codagnone <juan@zauberlabs.com>
2004-03-16 04:30:38 +0000

    actualizo los copyrights de los archivos que se modificaron en este an~o. conformidad entre todos

--- src/iol.c
+++ src/iol.c
@@ -1,7 +1,7 @@
 /*
  * iol.c -- IOLsucker web robot implementation
  *
- * Copyright (C) 2003 by Juan F. Codagnone <juam@users.sourceforge.net>
+ * Copyright (C) 2003, 2004 by Juan F. Codagnone <juam@users.sourceforge.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
